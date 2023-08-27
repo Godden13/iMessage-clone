719ff9a1ef3ef862b9e0832f98c039c7ea3f74fa
clint360 <clint360.rebase@gmail.com>
2023-08-27 20:12:31 +0100

    connected and commence stream configuration

--- app/index.tsx
+++ app/index.tsx
@@ -1,34 +1,10 @@
 import { StyleSheet, Text, View } from "react-native";
+import { Link, Redirect } from "expo-router";
 
 export default function Page() {
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello Clint</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
-    </View>
+    <Redirect href="/chat" />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
+const styles = StyleSheet.create({});
