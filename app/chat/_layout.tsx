6fc31c3c23a6fcd304c65cbe8bbe33375093f37e
clint360 <clint360.rebase@gmail.com>
2023-08-27 21:36:24 +0100

    coded the lists

--- app/chat/_layout.tsx
+++ app/chat/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack } from "expo-router";
 import { useEffect } from "react";
 import { StreamChat } from "stream-chat";
+import { OverlayProvider, Chat } from 'stream-chat-expo';
 
 const API_KEY = 'pqazq2amuu7t';
 const client = StreamChat.getInstance(API_KEY);
@@ -16,7 +17,7 @@ export default function ChatLayout() {
               name: "chia",
               image: 'https://i.imgur.com/fR9Jz14.png',
             },
-            'token'
+            client.devToken('chia')
           );
     
           const channel = client.channel('livestream', 'public', {
@@ -33,7 +34,13 @@ export default function ChatLayout() {
         };
       }, []);
 
-    return <Stack> 
+    return (
+    <OverlayProvider>
+        <Chat client={client}>
+    <Stack> 
         <Stack.Screen name="index" options={{title: "Messages"}} />   
     </Stack>
+    </Chat>
+    </OverlayProvider>
+    )
 }
--- app/chat/index.tsx
+++ app/chat/index.tsx
@@ -1,9 +1,15 @@
-import { View, Text } from "react-native";
+import { ChannelList } from "stream-chat-expo";
+import { useState } from "react";
+import { Channel as ChannelType } from "stream-chat";
+import { useRouter } from 'expo-router';
 
-export default function ChatScreen(){
+export default function ChatScreen() {
+  const router = useRouter();
+
+  const isPublic = { type: 'livestream' };
+
+  const [channel, setChannel] = useState<ChannelType>();
     return (
-        <View  style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-            <Text>Chat Screen</Text>
-        </View>
+        <ChannelList onSelect={(channel) => router.push(`/chat/channel/${channel.id}`)} />
     )
 }
